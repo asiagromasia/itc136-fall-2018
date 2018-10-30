e3f9e37f468fa45217aa974c9b2da3d34ba90667
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2018-10-29 20:49:46 -0700

    updated on website
    not command line

--- my_script.sh
+++ my_script.sh
@@ -5,3 +5,4 @@ echo "hello ITC-136"
 echo "Edited to github"
 echo"this is written in text editor and will be added via command line"
 
+echo"editing and adding from github> will commit first then git pull"
