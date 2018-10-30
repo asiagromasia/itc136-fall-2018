56aae1b250678896401d2fb8210a9521f10b7581
root <joanna.gromadzka@seattlecentral.edu>
2018-10-29 20:57:03 -0700

    added few commits

--- my_script.sh
+++ my_script.sh
@@ -7,4 +7,7 @@ echo"this is written in text editor and will be added via command line"
 
 echo"editing and adding from github> will commit first then git pull"
 
+echo"this is your current working directory" pwd
+echo"this is a version of linux you are running" uname -u
+echo"these are your logged in users" who 
 
